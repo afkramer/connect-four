3f57fa6c5ba8fd527b14a3f1ee8f7513dc75d2fb
Adriana Kramer <adri.kramer@gmail.com>
2022-10-19 21:21:33 +0200

    Make tests more readable and add test for purple diagonal win

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,17 +3,17 @@
 require_relative '../lib/board'
 
 describe Board do
-  pu = "\u2652"
-  ye = "\u264c"
+  pur = "\u2652"
+  yel = "\u264c"
 
   describe '#winner?' do
     context 'when there is no winner' do
       board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                     [' ', pu, pu, ' ', ' ', ' ', ' '],
-                     [' ', pu, ye, pu, ' ', ' ', ' '],
-                     [' ', ye, ye, pu, ' ', ' ', pu],
-                     [ye, pu, ye, ye, ye, ' ', ye],
-                     [ye, pu, pu, pu, ye, pu, pu]]
+                     [' ', pur, pur, ' ', ' ', ' ', ' '],
+                     [' ', pur, yel, pur, ' ', ' ', ' '],
+                     [' ', yel, yel, pur, ' ', ' ', pur],
+                     [yel, pur, yel, yel, yel, ' ', yel],
+                     [yel, pur, pur, pur, yel, pur, pur]]
       subject(:board_in_progress) { described_class.new(board_setup) }
 
       it 'returns false' do
@@ -25,9 +25,9 @@ describe Board do
       board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                     [' ', pu, pu, pu, pu, ' ', ' '],
-                     [' ', ye, ye, pu, ye, ' ', ' '],
-                     [' ', ye, ye, ye, pu, ' ', ' ']]
+                     [' ', pur, pur, pur, pur, ' ', ' '],
+                     [' ', yel, yel, pur, yel, ' ', ' '],
+                     [' ', yel, yel, yel, pur, ' ', ' ']]
       subject(:purple_win) { described_class.new(board_setup) }
 
       it 'returns true' do
@@ -39,9 +39,9 @@ describe Board do
       board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                     [pu, ye, ye, ye, ye, pu, pu],
-                     [ye, pu, pu, ye, pu, pu, ye],
-                     [ye, ye, pu, pu, ye, ye, pu]]
+                     [pur, yel, yel, yel, yel, pur, pur],
+                     [yel, pur, pur, yel, pur, pur, yel],
+                     [yel, yel, pur, pur, yel, yel, pur]]
       subject(:yellow_win) { described_class.new(board_setup) }
 
       it 'returns true' do
@@ -51,11 +51,11 @@ describe Board do
 
     context 'when there is a vertical win with all purple tokens' do
       board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                     [' ', ' ', ' ', ' ', pu, ' ', ' '],
-                     [' ', ' ', ' ', ' ', pu, ' ', ' '],
-                     [' ', ' ', ' ', ' ', pu, ' ', ' '],
-                     [' ', ' ', ' ', ' ', pu, ' ', ' '],
-                     [' ', ' ', ' ', ' ', ye, ' ', ' ']]
+                     [' ', ' ', ' ', ' ', pur, ' ', ' '],
+                     [' ', ' ', ' ', ' ', pur, ' ', ' '],
+                     [' ', ' ', ' ', ' ', pur, ' ', ' '],
+                     [' ', ' ', ' ', ' ', pur, ' ', ' '],
+                     [' ', ' ', ' ', ' ', yel, ' ', ' ']]
       subject(:purple_vert_win) { described_class.new(board_setup) }
 
       it 'returns true' do
@@ -64,16 +64,30 @@ describe Board do
     end
 
     context 'when there is a vertical win with all yellow tokens' do
-      board_setup = [[' ', ye, ' ', ' ', ' ', ' ', ' '],
-                     [' ', ye, ' ', ' ', ' ', ' ', ' '],
-                     [' ', ye, ' ', ' ', ' ', ' ', ' '],
-                     [' ', ye, ' ', ' ', ' ', ' ', ' '],
-                     [' ', pu, ' ', ' ', ' ', ' ', ' '],
-                     [' ', pu, ' ', ' ', ' ', ' ', ' ']]
+      board_setup = [[' ', yel, ' ', ' ', ' ', ' ', ' '],
+                     [' ', yel, ' ', ' ', ' ', ' ', ' '],
+                     [' ', yel, ' ', ' ', ' ', ' ', ' '],
+                     [' ', yel, ' ', ' ', ' ', ' ', ' '],
+                     [' ', pur, ' ', ' ', ' ', ' ', ' '],
+                     [' ', pur, ' ', ' ', ' ', ' ', ' ']]
       subject(:yellow_vert_win) { described_class.new(board_setup) }
 
       it 'returns true' do
         expect(yellow_vert_win.winner?).to be true
+      end
+    end
+
+    context 'when there is a purple diagonal win' do
+      board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                     [' ', pur, ' ', ' ', ' ', ' ', ' '],
+                     [' ', yel, pur, ' ', ' ', ' ', ' '],
+                     [' ', pur, yel, pur, ' ', ' ', ' '],
+                     [' ', pur, yel, yel, pur, ' ', ' ']]
+      subject(:purple_diag_win) { described_class.new(board_setup) }
+
+      it 'returns true' do
+        expect(purple_diag_win.winner?).to be true
       end
     end
   end
