9b2194f8ec735bcffebbd9c4ac5acc31919f4e84
Adriana Kramer <adri.kramer@gmail.com>
2022-10-19 21:13:46 +0200

    Make test cases realistic (um, gravity...), add test case for vertical yellow win

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,13 +7,27 @@ describe Board do
   ye = "\u264c"
 
   describe '#winner?' do
+    context 'when there is no winner' do
+      board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                     [' ', pu, pu, ' ', ' ', ' ', ' '],
+                     [' ', pu, ye, pu, ' ', ' ', ' '],
+                     [' ', ye, ye, pu, ' ', ' ', pu],
+                     [ye, pu, ye, ye, ye, ' ', ye],
+                     [ye, pu, pu, pu, ye, pu, pu]]
+      subject(:board_in_progress) { described_class.new(board_setup) }
+
+      it 'returns false' do
+        expect(board_in_progress.winner?).to be false
+      end
+    end
+
     context 'when there is a horizontal win with all purple tokens' do
       board_setup = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', pu, pu, pu, pu, ' ', ' '],
-                     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                     [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+                     [' ', ye, ye, pu, ye, ' ', ' '],
+                     [' ', ye, ye, ye, pu, ' ', ' ']]
       subject(:purple_win) { described_class.new(board_setup) }
 
       it 'returns true' do
@@ -26,26 +40,12 @@ describe Board do
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                      [pu, ye, ye, ye, ye, pu, pu],
-                     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                     [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+                     [ye, pu, pu, ye, pu, pu, ye],
+                     [ye, ye, pu, pu, ye, ye, pu]]
       subject(:yellow_win) { described_class.new(board_setup) }
 
       it 'returns true' do
         expect(yellow_win.winner?).to be true
-      end
-    end
-
-    context 'when there is no winner' do
-      board_setup = [[' ', ' ', ' ', ' ', ye, ' ', ' '],
-                     [' ', pu, pu, ' ', ' ', ' ', ' '],
-                     [' ', ' ', ' ', pu, ' ', ' ', ' '],
-                     [' ', ' ', ' ', pu, ' ', ' ', ' '],
-                     [ye, ' ', ye, ' ', ye, ' ', ye],
-                     [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-      subject(:board_in_progress) { described_class.new(board_setup) }
-
-      it 'returns false' do
-        expect(board_in_progress.winner?).to be false
       end
     end
 
@@ -55,11 +55,25 @@ describe Board do
                      [' ', ' ', ' ', ' ', pu, ' ', ' '],
                      [' ', ' ', ' ', ' ', pu, ' ', ' '],
                      [' ', ' ', ' ', ' ', pu, ' ', ' '],
-                     [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+                     [' ', ' ', ' ', ' ', ye, ' ', ' ']]
       subject(:purple_vert_win) { described_class.new(board_setup) }
 
       it 'returns true' do
         expect(purple_vert_win.winner?).to be true
+      end
+    end
+
+    context 'when there is a vertical win with all yellow tokens' do
+      board_setup = [[' ', ye, ' ', ' ', ' ', ' ', ' '],
+                     [' ', ye, ' ', ' ', ' ', ' ', ' '],
+                     [' ', ye, ' ', ' ', ' ', ' ', ' '],
+                     [' ', ye, ' ', ' ', ' ', ' ', ' '],
+                     [' ', pu, ' ', ' ', ' ', ' ', ' '],
+                     [' ', pu, ' ', ' ', ' ', ' ', ' ']]
+      subject(:yellow_vert_win) { described_class.new(board_setup) }
+
+      it 'returns true' do
+        expect(yellow_vert_win.winner?).to be true
       end
     end
   end
