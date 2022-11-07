b1aab2dbbf430cb684f7fab590ed4c9324e31ca3
Adriana Kramer <adri.kramer@gmail.com>
2022-11-07 07:28:21 +0100

    Add tests for Player.switch_turn

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class Player
+  attr_reader :has_turn
+
   def initialize(name, token)
     @name = name
     @token = token
+    @has_turn = token == "\u2652"
+  end
+
+  def switch_turn
+    @has_turn = !@has_turn
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,5 +3,24 @@
 require_relative '../lib/player'
 
 describe Player do
+  purp = "\u2652"
+  yell = "\u264c"
 
+  describe '#switch_turn' do
+    context 'the player is the active player' do
+      subject(:player_has_turn) { described_class.new('Martina', purp) }
+
+      it 'changes @has_turn from true to false' do
+        expect { player_has_turn.switch_turn }.to change(player_has_turn, :has_turn).from(true).to(false)
+      end
+    end
+
+    context 'the player is not the active player' do
+      subject(:player_does_not_have_turn) { described_class.new('Normi', yell) }
+
+      it 'changes @has_turn from false to true' do
+        expect { player_does_not_have_turn.switch_turn }.to change(player_does_not_have_turn, :has_turn).from(false).to(true)
+      end
+    end
+  end
 end
