f3d8c0310814f7b7bd7493425a3d5bb264857db2
Adriana Kramer <adri.kramer@gmail.com>
2022-11-07 06:58:08 +0100

    Add Gui.get_player_name

--- lib/gui.rb
+++ lib/gui.rb
@@ -3,6 +3,12 @@
 require_relative 'board'
 
 class Gui
+  def get_player_name(player_number, token)
+    puts "\nPlayer #{player_number}, you will play as #{token}."
+    puts "Please enter your name.\n\n"
+    gets.chomp
+  end
+
   def display_board(board)
     starting_row = "\n"
     (1..board[1].length).each { |num| starting_row << "  #{num}  " }
@@ -27,7 +33,7 @@ board_setup = [['  ', '  ', '  ', '  ', '  ', '  ', '  '],
                ['  ', yel, yel, pur, '  ', '  ', pur],
                [yel, pur, yel, yel, yel, '  ', yel],
                [yel, pur, pur, pur, yel, pur, pur]]
-board = Board.new(board_setup)
+#board = Board.new(board_setup)
 gui = Gui.new
-gui.display_board(board.board)
-
+#gui.display_board(board.board)
+gui.get_player_name(1, pur)
--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Player
+  def initialize(name, token)
+    @name = name
+    @token = token
+  end
 end
