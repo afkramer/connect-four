84551a6a8a9a2c977f4c2426d9e54e07363ee984
Adriana Kramer <adri.kramer@gmail.com>
2022-11-13 16:35:41 +0100

    Add more tests for invalid inputs to Game.valid_input?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/board'
 
 describe Game do
   describe '#valid_input?' do
@@ -9,6 +10,30 @@ describe Game do
       it 'returns true' do
         valid_input = 5
         expect(valid_game_input.valid_input?(valid_input)).to be true
+      end
+    end
+
+    context 'player inputs a number less than 1' do
+      subject(:game_input_too_low) { described_class.new }
+      it 'returns false' do
+        invalid_input = -1
+        expect(game_input_too_low.valid_input?(invalid_input)).to be false
+      end
+    end
+
+    context 'player inputs a number higher than width of board' do
+      subject(:game_input_too_high) { described_class.new }
+      it 'returns false' do
+        invalid_input = Board::WIDTH + 1
+        expect(game_input_too_high.valid_input?(invalid_input)).to be false
+      end
+    end
+
+    context 'player inputs a word' do
+      subject(:game_word_input) { described_class.new }
+      it 'returns false' do
+        invalid_input = 'one'
+        expect(game_word_input.valid_input?(invalid_input)).to be false
       end
     end
   end
