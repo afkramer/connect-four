abb35b38e5a5985d76b01b72485f4a68ad1cfb92
Adriana Kramer <adri.kramer@gmail.com>
2022-11-13 16:29:26 +0100

    Implement code for Game.valid_input?

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+require_relative 'board'
+
 class Game
+  def initialize
+    @board = Board.new
+  end
+
+  def valid_input?(input)
+    input.instance_of?(Integer) && input.positive? && input <= Board::WIDTH
+  end
 end
